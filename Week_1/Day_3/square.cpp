800010af6dab6723c82dc715080c5684e55fa3e4
sujal38-arch <sujal3802@gmail.com>
2026-06-03 18:19:31 +0530

    Add files via upload

---
+++ Week_1/Day_3/square.cpp
@@ -0,0 +1,9 @@
+vector<int> sortedSquares(vector<int>& nums) {
+        vector<int> temp;
+        int n = nums.size();
+        for(int i = 0; i < n; i++){
+            temp.push_back(nums[i] * nums[i]);
+        }
+        sort(temp.begin(), temp.end());
+        return temp;
+    }
