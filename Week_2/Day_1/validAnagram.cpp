9c937066ec38f9f35d2cec0e4b42dfcc4ba7d643
sujal38-arch <sujal3802@gmail.com>
2026-06-08 13:17:18 +0530

    Add files via upload

---
+++ Week_2/Day_1/validAnagram.cpp
@@ -0,0 +1,12 @@
+bool isAnagram(string s, string t){
+    if(s.size() != t.size()) return false;
+    vector<int> h(26);
+    for(int i = 0; i < s.size(); i++){
+        h[s[i] - 'a']++;
+    }
+    for(int i = 0; i < t.size(); i++){
+        int x = --h[t[i] - 'a'];
+        if(x < 0) return false;
+    }
+    return true;
+}
