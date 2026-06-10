8801ebe8ae6f28b949d9bd05290695c1ba3540c8
sujal38-arch <sujal3802@gmail.com>
2026-06-10 19:04:15 +0530

    Add files via upload

---
+++ Week_2/Day_3/isSubsequence.cpp
@@ -0,0 +1,15 @@
+bool isSubsequence(string s, string t) {
+        int m = s.size();
+        int n = t.size();
+        int i = 0, j = 0;
+        while(i < m && j < n){
+            if(s[i] == t[j]){
+                i++;
+            }
+            j++;
+        }
+        if(i == m){
+            return true;
+        }
+        return false;
+    }
