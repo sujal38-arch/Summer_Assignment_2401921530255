b57b96586c15e71a7dec94d3f474cf46326e977c
sujal38-arch <sujal3802@gmail.com>
2026-06-11 09:56:26 +0530

    Add files via upload

---
+++ Week_2/Day_4/reverseWords3.cpp
@@ -0,0 +1,14 @@
+string reverseWords(string s) {
+        int n = s.size();
+        for(int i = 0; i < n; i++){
+            if(s[i] != ' '){
+                int j = i;
+                while(j < n && s[j] != ' '){
+                    j++;
+                }
+                reverse(begin(s) + i, begin(s) + j);
+                i = j;
+            }
+        }
+        return s;
+    }
