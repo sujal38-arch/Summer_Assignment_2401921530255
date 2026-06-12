a4f19dd8e94cc0678d531153e06853e7c0fe6bff
sujal38-arch <sujal3802@gmail.com>
2026-06-12 23:58:16 +0530

    Add files via upload

---
+++ Week_2/Day_5/longestPalindrome.cpp
@@ -0,0 +1,25 @@
+bool solve(string &s, int i, int j){
+    if(i >= j){
+        return true;
+    }
+    if(s[i] == s[j]){
+        return solve(s, i + 1, j - 1);
+    }
+    return false;
+}
+string longestPalindrome(string s){
+    int n = s.size();
+    int maxlen = INT_MIN;
+    int sp = 0;
+    for(int i = 0; i < n; i++){
+        for(int j = i; j < n; j++){
+            if(solve(s, i, j) == true){
+                if(j - i + 1 > maxlen){
+                    maxlen = j - i + 1;
+                    sp = i;
+                }
+            }
+        }
+    }
+    return s.substr(sp, maxlen);
+}
