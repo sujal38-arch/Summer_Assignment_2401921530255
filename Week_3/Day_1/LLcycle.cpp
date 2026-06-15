adbe154f7c6340b21e1da14d7970402037d99b7a
sujal38-arch <sujal3802@gmail.com>
2026-06-15 14:23:45 +0530

    Add files via upload

---
+++ Week_3/Day_1/LLcycle.cpp
@@ -0,0 +1,11 @@
+bool hasCycle(ListNode *head){
+    if(head == NULL) return false;
+    ListNode *slow = head;
+    ListNode *fast = head -> next;
+    while(fast != NULL && fast -> next != NULL){
+        slow = slow -> next;
+        fast = fast -> next -> next;
+        if(slow == fast) return true;
+    }
+    return false;
+}
