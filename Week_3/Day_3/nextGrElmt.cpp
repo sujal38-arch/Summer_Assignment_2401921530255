7017293a7d07d5b5da7771d8bd77718836c06cdf
sujal38-arch <sujal3802@gmail.com>
2026-06-17 13:06:55 +0530

    Add files via upload

---
+++ Week_3/Day_3/nextGrElmt.cpp
@@ -0,0 +1,22 @@
+vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2){
+    stack<int> st;
+    unordered_map<int, int> mpp;
+    int n = nums2.size();
+    for(int i = n - 1; i >= 0; i--){
+        while(!st.empty() && st.top() <= nums2[i]){
+            st.pop();
+        }
+        if(st.empty()){
+            mpp[nums2[i]] = -1;
+        }
+        else{
+            mpp[nums2[i]] = st.top();
+        }
+        st.push(nums2[i]);
+    }
+    vector<int> ans;
+    for(int i = 0; i < nums1.size(); i++){
+        ans.push_back(mpp[nums1[i]]);
+    }
+    return ans;
+}
