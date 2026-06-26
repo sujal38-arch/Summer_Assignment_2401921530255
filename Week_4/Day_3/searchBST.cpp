ad05723e940d973aebd9c4a73e821ac2ebe89881
sujal38-arch <sujal3802@gmail.com>
2026-06-26 22:17:09 +0530

    Add files via upload

---
+++ Week_4/Day_3/searchBST.cpp
@@ -0,0 +1,6 @@
+TreeNode* searchBST(TreeNode* root, int val) {
+        while(root != NULL && root -> val != val){
+            root = val < root -> val? root -> left : root -> right;
+        }
+        return root;
+    }
